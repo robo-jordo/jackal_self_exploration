b3ecc47889a7f55cc2c90542f07ffcdc5302b8d6
robo-jordo <jordhask@gmail.com>
2019-12-09 14:51:50 -0600

    logging and csv storing

--- src/algo.py
+++ src/algo.py
@@ -6,8 +6,8 @@ import ml_lib as ml
 import collections
 import random
 import sys
-from memory_profiler import profile
 import gc
+import csv
 
 import roslaunch
 from std_msgs.msg import String
@@ -24,7 +24,7 @@ import resource
 
 
 # global variables
-learning_rate = 0.001
+learning_rate = 0.0001
 action_size = 8
 state_size = 8
 epsilon = 1
@@ -33,13 +33,13 @@ epsilon_decay = 0.995
 gamma = 0.95
 
 rospy.init_node('RL', anonymous=True)
-memory = collections.deque(maxlen=40)
+memory = collections.deque(maxlen=4000)
 
 # Create openai environment
 env = ml.MachineLearning()
 
 # env = gym.make("CartPole-v0")
-# Reset environment 
+# Reset environment
 observation = env.reset()
 
 # Neural Net for Deep Q Learning
@@ -47,17 +47,18 @@ observation = env.reset()
 model = Sequential()
 # 'Dense' is the basic form of a neural network layer
 # Input Layer of state size(4) and Hidden Layer with 24 nodes
-model.add(Dense(48, input_dim=state_size, activation='relu'))
+model.add(Dense(8, input_dim=state_size, activation='tanh'))
 # Hidden layer with 24 nodes
-model.add(Dense(48, activation='relu'))
-
-model.add(Dense(24, activation='relu'))
+#model.add(Dense(24, activation='relu'))
 # Output Layer with # of actions: 2 nodes (left, right)
 model.add(Dense(action_size, activation='linear'))
 # Create the model based on the information above
 model.compile(loss='mse',optimizer=optimizers.Adam(lr=learning_rate))
+#model.load_weights('weights/2157_my_model_weights.h5')
 
 indo = {"N":0,"NW":1,"W":2,"SW":3,"S":4,"SE":5,"E":6,"NE":7}
+
+log_values = []
 
 def sizeof_fmt(num, suffix='B'):
 	''' by Fred Cirera,  https://stackoverflow.com/a/1094933/1870254, modified'''
@@ -69,6 +70,7 @@ def sizeof_fmt(num, suffix='B'):
 
 
 def act(state):
+	global log_values
 	if np.random.rand() <= epsilon:
 		# The agent acts randomly
 		return np.random.choice(env.actions)
@@ -76,6 +78,7 @@ def act(state):
 	# Predict the reward value based on the given state
 	act_values = model.predict(state)
 	# Pick the action based on the predicted reward
+	log_values.append(env.actions[np.argmax(act_values[0])])
 	return env.actions[np.argmax(act_values[0])]
 
 def remember(state, action, reward, next_state, done):
@@ -91,6 +94,8 @@ def replay(batch_size):
 			target_f = model.predict(state)
 
 			target_f[0][indo[action]] = target
+			#print("state: " + str(state))
+			#print("target: " + str(target_f))
 			model.fit(state, target_f, epochs=1, verbose=0)
 		if epsilon > epsilon_min:
 			epsilon *= epsilon_decay
@@ -98,23 +103,34 @@ def replay(batch_size):
 # Loop through episodes
 
 def main():
+	global epsilon
+	global log_values
 	for e in range(3000):
+		log_values = []
 		state = env.reset()
 		if (state == [0]*state_size):
 			print("points not being published")
 		#assert(state != [0]*state_size)
 		state = np.reshape(state, [1, 8])
 		running_rew = 0
+
+		if epsilon == 0:
+			epsilon =  stored_eps
+			print("reverting to training")
+		if ((e % 10)==0 and e > 0 ):
+			stored_eps = epsilon
+			epsilon = 0
+			print("Trying policy")
 		for t in range(2000):
 			result = gc.collect()
-			print(str(t+1)+"/30")
+			print(str(t+1)+"/100")
 			sys.stdout.write("\033[F")
 			state = env.get_observation()
 			state = np.reshape(state, [1, 8])
 
 			action = act(state)
 			#print '1 Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-
+			#print("action: "+str(action))
 			observation, reward, done, info = env.step(action)
 			#print '2 Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 			observation = np.reshape(observation, [1, 8])
@@ -124,15 +140,17 @@ def main():
 
 			state = observation
 
-			if done or t == 50:
-				print("episode: {}, score: {}, eps: {}, memory: {}".format(e, running_rew, epsilon, len(memory)))
+			if done or t == 99:
+				print("episode: {}, score: {}, eps: {}, memory: {}, collisions: {}".format(e, running_rew, epsilon, len(memory), env.collisions))
 				# observation = env.reset()
 				break
 
-		if len(memory) > 32:
-			replay(32)
+		if len(memory) > 84:
+			replay(84)
 		model.save_weights("weights/"+str(e)+'_my_model_weights.h5')
-
+		with open('moves/'+str(e) +'_file.csv', mode='w') as moves_file:
+    			employee_writer = csv.writer(moves_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    			employee_writer.writerow(log_values)
 main()
-		
+
 #env.close()
