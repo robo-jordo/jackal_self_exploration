38494733726240d22462089a1961975f510b7903
Jordan Haskel <jordhask@gmail.com>
2019-12-12 22:58:04 -0600

    fix indent

--- src/points.py
+++ src/points.py
@@ -43,124 +43,124 @@ forward_shift = {"N":0,"NW":1,"W":2,"SW":3,"S":4,"SE":5,"E":6,"NE":7}
 # Segment min and max angle value calculation
 segments = []
 for i in range(int(grid_resolution)):
-    segments.append(-3.14+6.28*((1+i*2)/(grid_resolution*2)))
+	segments.append(-3.14+6.28*((1+i*2)/(grid_resolution*2)))
 
 # function to help shift lists
 def rotate(l, n):
-    """ Function to rotate entries in list "l" by n positions
+	""" Function to rotate entries in list "l" by n positions
 
-        Args:
-            l (list): list to be rotated
-            n (int): positions to rotate by
+		Args:
+			l (list): list to be rotated
+			n (int): positions to rotate by
 
-        Returns:
-            new_l (list): the rotated list
+		Returns:
+			new_l (list): the rotated list
 
-    """
-        return l[n:] + l[:n]
+	"""
+	return l[n:] + l[:n]
 
 def _heading_callback(data):
-    """ Callback to get heading direction of robot and store it
+	""" Callback to get heading direction of robot and store it
 
-        Args:
-            data: ros message of String
+		Args:
+			data: ros message of String
 
-        Returns:
-            None
+		Returns:
+			None
 
-        """
-    global heading 
-    heading = data.data
+		"""
+	global heading 
+	heading = data.data
 
 def _laser_callback(data):
-    """ Callback to laserscan data and segment it into 8 equal segments
-        The callback also publishes these 8 segments along with the averages
-        of each segment, and publishes the segment corresponding to the front
-        of the robot to its own topci
+	""" Callback to laserscan data and segment it into 8 equal segments
+		The callback also publishes these 8 segments along with the averages
+		of each segment, and publishes the segment corresponding to the front
+		of the robot to its own topci
 
-        Args:
-            data: ros message of type nav_msgs/Odometry
+		Args:
+			data: ros message of type nav_msgs/Odometry
 
-        Returns:
-            None
+		Returns:
+			None
 
-        """
-    global scan
-    # Check we have a heading
-    if (heading == ""):
-        return -1
+		"""
+	global scan
+	# Check we have a heading
+	if (heading == ""):
+		return -1
 
-    # Rotate segments to match co-ordinate system
-    segments_temp = rotate(segments,shift[heading])
-    current_time = rospy.Time.now()
+	# Rotate segments to match co-ordinate system
+	segments_temp = rotate(segments,shift[heading])
+	current_time = rospy.Time.now()
 
-    for i in range(int(grid_resolution)):
-        # Calculate how many points in each segment
-        scan_step_size = len(data.ranges)/grid_resolution
+	for i in range(int(grid_resolution)):
+		# Calculate how many points in each segment
+		scan_step_size = len(data.ranges)/grid_resolution
 
-        # Fill laser scan header
-        scan.header.stamp = current_time
-        scan.header.frame_id = data.header.frame_id
+		# Fill laser scan header
+		scan.header.stamp = current_time
+		scan.header.frame_id = data.header.frame_id
 
-        # Fill in scan min and max angles for each segment
-        if i<grid_resolution-1:
-            scan.angle_min = segments_temp[i]
-            scan.angle_max = segments_temp[i+1]
-        else:
-            scan.angle_min = segments_temp[-1]
-            scan.angle_max = segments_temp[0]
-        scan.angle_increment = data.angle_increment
-        scan.time_increment = data.time_increment
-        scan.scan_time = data.scan_time
-        scan.range_min = 0.0
-        scan.range_max = 100.0
+		# Fill in scan min and max angles for each segment
+		if i<grid_resolution-1:
+			scan.angle_min = segments_temp[i]
+			scan.angle_max = segments_temp[i+1]
+		else:
+			scan.angle_min = segments_temp[-1]
+			scan.angle_max = segments_temp[0]
+		scan.angle_increment = data.angle_increment
+		scan.time_increment = data.time_increment
+		scan.scan_time = data.scan_time
+		scan.range_min = 0.0
+		scan.range_max = 100.0
 
-        # Clear data from scan message
-        scan.ranges = []
-        scan.intensities = []
+		# Clear data from scan message
+		scan.ranges = []
+		scan.intensities = []
 
-        # shift data so that octals always match same global position
-        temp_data = rotate(data.ranges, int(scan_step_size*shift[heading]))
+		# shift data so that octals always match same global position
+		temp_data = rotate(data.ranges, int(scan_step_size*shift[heading]))
 
-        # Fill in range data for each segment
-        if i<grid_resolution-1:
-            for j in range(int(i*scan_step_size+(0.5)*scan_step_size),int((i+1)*scan_step_size+(0.5)*scan_step_size)):
-                scan.ranges.append(temp_data[j]) 
-                #scan.ranges.append(data.ranges[j])
-                scan.intensities.append(1)  # fake data
-            pubs[i].publish(scan)
-        else:
-            for j in range(int(i*scan_step_size+(0.5)*scan_step_size),int((i+1)*scan_step_size)):
-                scan.ranges.append(temp_data[j])
-                #scan.ranges.append(data.ranges[j])
-                scan.intensities.append(1)  # fake data
-            for j in range(0,int(0.5*scan_step_size)):
-                scan.ranges.append(temp_data[j])
-                #scan.ranges.append(data.ranges[j])
-                scan.intensities.append(1)  # fake data
-            pubs[i].publish(scan)
+		# Fill in range data for each segment
+		if i<grid_resolution-1:
+			for j in range(int(i*scan_step_size+(0.5)*scan_step_size),int((i+1)*scan_step_size+(0.5)*scan_step_size)):
+				scan.ranges.append(temp_data[j]) 
+				#scan.ranges.append(data.ranges[j])
+				scan.intensities.append(1)  # fake data
+			pubs[i].publish(scan)
+		else:
+			for j in range(int(i*scan_step_size+(0.5)*scan_step_size),int((i+1)*scan_step_size)):
+				scan.ranges.append(temp_data[j])
+				#scan.ranges.append(data.ranges[j])
+				scan.intensities.append(1)  # fake data
+			for j in range(0,int(0.5*scan_step_size)):
+				scan.ranges.append(temp_data[j])
+				#scan.ranges.append(data.ranges[j])
+				scan.intensities.append(1)  # fake data
+			pubs[i].publish(scan)
 
-        # Calculate metrics on each segment
-        scan_averages[i] = np.mean(scan.ranges)
-        scan_mins[i] = np.min(scan.ranges)
-        scan_maxs[i] = np.max(scan.ranges)
+		# Calculate metrics on each segment
+		scan_averages[i] = np.mean(scan.ranges)
+		scan_mins[i] = np.min(scan.ranges)
+		scan_maxs[i] = np.max(scan.ranges)
 
-        # Publish the laser scan data from the front of the robot
-        if (i == forward_shift[heading]):
-            pubfront.publish(scan)
-    pubav.publish("&".join(map(str,scan_averages)))
+		# Publish the laser scan data from the front of the robot
+		if (i == forward_shift[heading]):
+			pubfront.publish(scan)
+	pubav.publish("&".join(map(str,scan_averages)))
 
 
 
 def listener():
-    # init node 
-    rospy.init_node('point_publisher', anonymous=True)
+	# init node 
+	rospy.init_node('point_publisher', anonymous=True)
 
-    rospy.Subscriber("front/scan", LaserScan, _laser_callback)
-    rospy.Subscriber("heading", String, _heading_callback)
+	rospy.Subscriber("front/scan", LaserScan, _laser_callback)
+	rospy.Subscriber("heading", String, _heading_callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+	listener()
