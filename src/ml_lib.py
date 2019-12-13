f92c91fe5d3fd68cae4a7555575e72c357433158
Jordan Haskel <jordhask@gmail.com>
2019-12-12 21:39:03 -0600

    commented and cleaned code

--- src/ml_lib.py
+++ src/ml_lib.py
@@ -9,6 +9,8 @@ import roslaunch
 import sys
 import tf
 import random
+import rospkg
+import resource
 
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState, GetModelState
@@ -18,28 +20,16 @@ from geometry_msgs.msg import Pose, PoseWithCovarianceStamped
 from nav_msgs.msg import OccupancyGrid, Odometry
 from gazebo_msgs.srv import DeleteModel, SpawnModel
 from controller_manager_msgs.srv import LoadController, UnloadController, SwitchController
-import resource
-
-# function to assist in checking if two circular states are equal
-def circular_equality(states, observation):
-	exists =  False
-	index = -1
-	doubled_obs = np.concatenate((observation, observation))
-	print(doubled_obs)
-	doubled_obs = ''.join(map(str, doubled_obs))
-	for i in range(len(states)):
-		comp_string = ''.join(map(str, states[i]))
-		if comp_string in doubled_obs:
-			exists = True
-			index = i
-			break
-	return exists, index
 
 class MachineLearning:
 
 	def __init__(self):
 		self.listener()
 
+	rospack = rospkg.RosPack()
+
+	# Publisher to reset gmapping each episode
+	pub = rospy.Publisher('/syscommand', String, queue_size=1)
 
 	# Gazebo services
 	set_gaz_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
@@ -52,19 +42,11 @@ class MachineLearning:
 	switch_controller = rospy.ServiceProxy('controller_manager/switch_controller', SwitchController)
 
 	# URDF and pose variables
-
-	p = os.popen("~/jackal_ws/src/jackal/jackal_description/scripts/env_run ~/jackal_ws/src/jackal/jackal_description/urdf/configs/front_laser rosrun xacro xacro.py " + "~/jackal_ws/src/jackal/jackal_description/urdf/jackal.urdf.xacro")
+	p = os.popen(rospack.get_path('jackal_description')+'/scripts/env_run '+rospack.get_path('jackal_description')+'/urdf/configs/front_laser rosrun xacro xacro.py ' + rospack.get_path('jackal_description') +'/urdf/jackal.urdf.xacro')
 	xml_string = p.read()
 	p.close()
-	pose = Pose()
-	pose.position.x = 0
-	pose.position.y = 0
-	pose.position.z = 0.2
-	pose.orientation.x = 0
-	pose.orientation.y = 0
-	pose.orientation.z = 0
-	pose.orientation.w = 1
 
+	# Empty pose for ekf localization set pose
 	pose2 = PoseWithCovarianceStamped()
 	pose2.pose.pose.position.x = 0
 	pose2.pose.pose.position.y = 0
@@ -77,6 +59,7 @@ class MachineLearning:
 		0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 		0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
+	# Empty pose for gazebo set_model_state
 	state_msg = ModelState()
 	state_msg.model_name = 'jackal'
 	state_msg.pose.position.x = 0
@@ -87,20 +70,9 @@ class MachineLearning:
 	state_msg.pose.orientation.z = 0
 	state_msg.pose.orientation.w = 1
 
+	# List of valid start points in jackal race world
+	random_spawns = 1
 	start_points = [(-2,-1.15),(-4.5,-1.3),(-4.5,8.2),(-1.9,1.6),(5.5,-5.3),(5.75,-8.5),(-6.75,-4.5)]
-
-	# Roslaunch api usage for gmapping reset
-
-	pub = rospy.Publisher('/syscommand', String, queue_size=1)
-	# uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-	# roslaunch.configure_logging(uuid)
-	# launch = roslaunch.parent.ROSLaunchParent(uuid, ['/home/jordan/jackal_ws/src/jackal_exploration/launch/gmapping_no_output.launch'])
-	# launch.start()
-	# cli_args = ['/home/jordan/jackal_ws/src/jackal_exploration/launch/gmapping_no_output.launch']
-	# roslaunch_args = cli_args[1:]
-	# roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
-
-	# parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
 
 	# Global variables and data structures
 
@@ -109,10 +81,8 @@ class MachineLearning:
 	observation = np.array([0,0,0,0,0,0,0,0])
 	scans = [0,0,0,0,0,0,0,0]
 
-	episodes = 3
-	discount = 0.5
-	alpha = 0.1
 	collisions = 0
+
 
 	information_metric = 0
 	old_info_metric = 0
@@ -121,11 +91,8 @@ class MachineLearning:
 	obs_type = "scan"
 
 	# Import movement object to move model
-
 	new_movement = move.Movement()
 	new_movement.listener()
-
-	odom_broadcaster = tf.TransformBroadcaster()
 
 	mi = mapper.MapImage()
 	mi.listener()
@@ -133,41 +100,39 @@ class MachineLearning:
 
 	# Callbacks
 	def _avs_callback(self,data):
-		""" Function to spawn new model and controllers in gazebo and start the controllers.
+		""" Function to take in the average lidar values produced by the points node
+			and split it into 8 values.
 
 		Args:
-			param1: The first parameter.
-			param2: The second parameter.
+			data: The string data passed from the points node
 
 		Returns:
-			The return value. True for success, False otherwise.
+			None
 
 		"""
 		self.scans = str(data.data).split("&")
 
 	def _pos_callback(self,data):
-                """ Function to spawn new model and controllers in gazebo and start the controllers.
-
-                Args:
-                        param1: The first parameter.
-                        param2: The second parameter.
-
-                Returns:
-                        The return value. True for success, False otherwise.
-
-                """
-		if data.child_frame_id == 'base_link':
-                	self.current_co_ord = [data.pose.pose.position.x , data.pose.pose.position.y]
-
-	def _reward_callback(self, data):
-		""" Function to spawn new model and controllers in gazebo and start the controllers.
+		""" Function to get the x,y co-ords of the robot from the odometry/filtered topic.
 
 		Args:
-			param1: The first parameter.
-			param2: The second parameter.
+			data: ros message of type nav_msgs/Odometry
 
 		Returns:
-			The return value. True for success, False otherwise.
+			None
+
+		"""
+		if data.child_frame_id == 'base_link':
+			self.current_co_ord = [data.pose.pose.position.x , data.pose.pose.position.y]
+
+	def _reward_callback(self, data):
+		""" Function to calculate how many blocks of the map have known contents
+
+		Args:
+			data: ros messsage of type nav_msgs/OccupancyGrid
+
+		Returns:
+			None
 
 		"""
 		map_data = np.array(data.data)
@@ -177,33 +142,35 @@ class MachineLearning:
 	# function to set up callbacks
 	def listener(self):
 		rospy.Subscriber("/map", OccupancyGrid, self._reward_callback)
-		rospy.Subscriber("scan_avs", String, self._avs_callback)
-		#rospy.Subscriber("/odometry/filtered", Odometry, self._pos_callback)
+		rospy.Subscriber("/scan_avs", String, self._avs_callback)
+		rospy.Subscriber("/odometry/filtered", Odometry, self._pos_callback)
 
 	# Usable methods from this class to assist machine learning algorithms
-	#@profile
-	def new_model(self):
-		""" Function to spawn new model and controllers in gazebo and start the controllers.
+
+	def _new_model(self):
+		""" Function to reset model state in gazebo and ekf_localization.
+		    This function also resets the map being output by gmapping
 
 		Args:
-			param1: The first parameter.
-			param2: The second parameter.
+			None
 
 		Returns:
-			The return value. True for success, False otherwise.
+			None
 
 		"""
 		rospy.wait_for_service("gazebo/set_model_state", timeout=20)
-		#rospy.wait_for_service("set_pose", timeout=20)
+
 		self.pose2.header.stamp = rospy.Time.now()
 
-		start_pose = random.choice(self.start_points)
+		# Choose random point from defined safe respawn points
+		if self.random_spawns:
+			start_pose = random.choice(self.start_points)
 
-		self.state_msg.pose.position.x = start_pose[0]
-	        self.state_msg.pose.position.y = start_pose[1]
+			self.state_msg.pose.position.x = start_pose[0]
+			self.state_msg.pose.position.y = start_pose[1]
 
-		self.pose2.pose.pose.position.x = start_pose[0]
-        	self.pose2.pose.pose.position.y = start_pose[1]
+			self.pose2.pose.pose.position.x = start_pose[0]
+			self.pose2.pose.pose.position.y = start_pose[1]
 
 		# Reset Gazebo
 		resp = self.set_gaz_state( self.state_msg )
@@ -214,40 +181,51 @@ class MachineLearning:
 		# Reset Gmapping map
 		self.pub.publish("reset")
 
+		# pause for map to clear
 		rospy.sleep(2)
 
-
+		# reset information metrics
 		self.information_metric = 0
 		self.old_info_metric = 0
-		#print("New Jackal ready")
+
 
 	def get_observation(self):
-		""" Function to spawn new model and controllers in gazebo and start the controllers.
+		""" Get observation of the relevant type 
 
 		Args:
-			param1: The first parameter.
-			param2: The second parameter.
+			None
 
 		Returns:
-			The return value. True for success, False otherwise.
+			Observation: This observation will be of the type specified by the
+						 obs_type attribute of this class.
+						 - for obs_type = scan: list of 8 floats (8 scan segment averages)
+						 - for obs_type = pos: list of co-ordinates of the form [x,y]
+						 - for obs_type = img: 64x64 numpy array representing an image of 
+						                       the robots current understanding of the map
+
 
 		"""
+
+		# Observation for scan type
 		if (self.obs_type == "scan"):
 			temp = self.scans
 			for scan in range(len(temp)):
-				#print(temp[scan])
 				if float(temp[scan]) >100:
 					temp[scan] = 0
-					#print("GOT ONE")
 				else:
 					temp[scan] = float(temp[scan])
-			#print("temp" + str(temp))
 			return temp
+
+		# observation for pos type
 		elif (self.obs_type == "pos"):
 			object_coords = self.get_gaz_state("jackal","")
 			return [object_coords.pose.position.x , object_coords.pose.position.y]
+		
+		# Observation for img type
 		elif (self.obs_type == "img"):
 			return self.mi.horizontal_img
+
+		# Catch all for problems seting obs_type
 		else:
 			print("Set observation type") 
 
@@ -258,8 +236,8 @@ class MachineLearning:
 			action (str): The direction in which to move
 
 		Returns:
-			None
-
+			result (int): -1 if the requested move would have cause a collision
+ 						   1 if the requested move was succesful
 		"""
 		result = self.new_movement.move(action)
 		return result
@@ -286,12 +264,12 @@ class MachineLearning:
 								of each lidar segment
 
 		"""
-		self.new_model()
+		self._new_model()
 		self.collisions = 0
 		return self.get_observation()
 
 	def step(self, action):
-		""" Function to move model and return useful info like the OpenAi gym
+		""" Function to move model and return useful info (like the OpenAi gym does)
 
 		Args:
 			action (str): The direction in which to move
@@ -301,37 +279,25 @@ class MachineLearning:
 								of each lidar segment
 			value (int): The new information gathered in the map
 			done (bool): Whether the simulation in a termination state
-			info (int): ???? Nothing yet just mirrors OpenAi gym setup
+			info (int): Nothing yet just mirrors OpenAi gym setup
 
 		"""
+
 		result = self.move_model(action)
 		reward = self.delta_score()
 
+		# set the reward scalar here 
+		#############################
 		if reward <5:
 			reward = -0.5
 		else:
 			reward = 0.5
 		if result == -1:
 			reward = -1
+		#############################
+
+		# Increment collisions 
+		if result == -1:
 			self.collisions = self.collisions + 1
-		# else:
-		# 	reward = 1
 		return self.get_observation(), reward, 0, 0
 
-# Test case stuff
-observation = np.array([2,1,5,1,1,3,3])
-states = np.array([[1,3,2,2,1,5,1],[1,3,2,1,1,5,1],[1,3,3,2,1,5,1]])
-
-def numpy_test():
-
-	Qs = np.array([[[1,3,2,2,1,5,1],"NW",2,1],
-				   [[1,3,2,1,1,5,1],"N",3,2],
-				   [[1,3,3,2,1,5,1],"W",3,1]])
-	print(Qs[:,0])
-	print(circular_equality(Qs[:,0],observation))
-
-	Qs = np.vstack((Qs, [[1,3,2,2,1,5,5],"S",3,1]))
-	print(Qs)
-
-if __name__ == '__main__':
-	numpy_test()
